7bf5cad62a580ef9e60a3ba0f923366c0a77d1b6
Luis Alberto Osorio Lomeña <osoriolomena@gmail.com>
2021-09-08 18:07:50 -0400

    Dojo Javascript Jugando con objetos

---
+++ javascript_rec/jugando_con_objetos.js
@@ -0,0 +1,28 @@
+// Jugando con Objetos
+// Objetivos
+// Practica iterar por medio de un array de objetos/diccionarios.
+// Imagina que se te entrega un array de objetos. Por ejemplo,
+
+// var users = [{name: "Michael", age:37}, {name: "John", age:30}, {name: "David", age:27}];
+// ¿Cómo harías print/log de la edad de John?
+// ¿Cómo harías print/log del nombre del primer objeto?
+// ¿Cómo harías print/log del nombre y la edad de cada usuario utilizando un for loop? Tu output debería verse algo como esto
+// ¿Cómo harías para imprimir el nombre de los mayores de edad?
+// Michael - 37
+// John - 30
+// David - 27
+
+var users = [{name: "Michael", age:37}, {name: "John", age:30}, {name: "David", age:27}];
+
+// 1. edad de Johon
+console.log(users[1].age);
+
+
+// 2. nombre del primer objeto
+console,log(users.[0].name)
+
+// 3.- nombre y la edad de cada usuario
+
+for (var i = 0; i <= users.length; i++){
+  console.log(usr.[i].name, "-", users[i].age);
+} 
--- javascript_rec/los 13 algoritmos basicos.js
+++ javascript_rec/los 13 algoritmos basicos.js
@@ -129,7 +129,7 @@ function impArr() {
 
 function arrmenor(arr, Y) {
     var count = 0;
-    for (i = 0; i <= arr.length; i++){
+    for (var i = 0; i <= arr.length; i++){
         if (arr[i] > Y){
             count++
         }   
@@ -149,3 +149,4 @@ function cuadArr(arr) {
     }
     return arr;
 }
+
